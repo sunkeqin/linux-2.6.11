1777272f22f1e23c7fc6c07cec88e0aad3c3cb80
Sun Keqin <jimmysun1988@qq.com>
2015-07-03 14:42:05 +0800

    add files

---
+++ include/linux/compile.h
@@ -0,0 +1,8 @@
+/* This file is auto generated, version 0 */
+#define UTS_MACHINE "i386"
+#define UTS_VERSION "#0 SMP Wed Jul 1 09:34:46 CST 2015"
+#define LINUX_COMPILE_TIME "09:34:46"
+#define LINUX_COMPILE_BY "keqin"
+#define LINUX_COMPILE_HOST "keqin-virtual-machine"
+#define LINUX_COMPILE_DOMAIN ""
+#define LINUX_COMPILER "gcc version 4.8.4 (Ubuntu 4.8.4-2ubuntu1~14.04) "
